b71fce8de1c2ff0fb124d69e779ed3679e26b550
Leela Mohan Gongunta <lmohan@lmohan-ltm5vno.internal.salesforce.com>
2022-08-03 11:33:30 +0530

    Change titles and styling on homepage

---
+++ config.js
@@ -0,0 +1,7 @@
+/*exports.USERNAME = process.env.USERNAME;
+exports.PASSWORD = process.env.PASSWORD;
+exports.SECURITYTOKEN = process.env.SECURITYTOKEN;*/
+
+exports.USERNAME = 'user9@sdo.demo';
+exports.PASSWORD = 'salesforce1';
+exports.SECURITYTOKEN = '';
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+var jsforce = require('jsforce');
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+var channel = '/data/CaseChangeEvent';
+var user = config.USERNAME;
+var pass = config.PASSWORD;
+var securityToken = config.SECURITYTOKEN;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -10,6 +15,30 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
+  });
+});
+
+conn.login(user, pass + securityToken, function (err, res) {
+  console.log('loggedin');
+  if (err) {
+    return console.error(err);
+  }
+
+  var client = conn.streaming.createClient([
+    new jsforce.StreamingExtension.Replay(channel, replayId),
+    new jsforce.StreamingExtension.AuthFailure(function () {
+      console.log('failed');
+      return process.exit(1);
+    }),
+  ]);
+
+  subscription = client.subscribe(channel, function (data) {
+    console.log('Received CDC Event');
+    console.log(JSON.stringify(data));
+    console.log('************ STATUS ***************');
+    console.log(JSON.stringify(data.payload.Status));
+    //socket.send(JSON.stringify(data));
+    console.log('Data sent to clients!!');
   });
 });
 
