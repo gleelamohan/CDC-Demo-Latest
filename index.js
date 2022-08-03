2a91e845ca8593188e11b61693f10e46033a939b
Leela Mohan Gongunta <lmohan@lmohan-ltm5vno.internal.salesforce.com>
2022-08-03 11:34:48 +0530

    Change titles and styling on homepage

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+var config = require('./config.js');
 var channel = '/data/CaseChangeEvent';
 var user = config.USERNAME;
 var pass = config.PASSWORD;
