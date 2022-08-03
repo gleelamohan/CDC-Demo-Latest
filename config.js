1c5a44e72c8f9830895c13835f213f6903a3443c
Leela Mohan Gongunta <lmohan@lmohan-ltm5vno.internal.salesforce.com>
2022-08-03 17:17:01 +0530

    Change titles and styling on homepage

--- config.js
+++ config.js
@@ -1,7 +1,3 @@
-/*exports.USERNAME = process.env.USERNAME;
+exports.USERNAME = process.env.USERNAME;
 exports.PASSWORD = process.env.PASSWORD;
-exports.SECURITYTOKEN = process.env.SECURITYTOKEN;*/
-
-exports.USERNAME = 'user9@sdo.demo';
-exports.PASSWORD = 'salesforce1';
-exports.SECURITYTOKEN = '';
+exports.SECURITYTOKEN = process.env.SECURITYTOKEN;
